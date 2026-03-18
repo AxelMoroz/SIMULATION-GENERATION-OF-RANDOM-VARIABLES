d526526c15541f780bf978c3343cbf1e600207c3
Axel Moroz <moroz_axel@hotmail.com>
2026-03-18 00:22:28 -0300

    Add files via upload

---
+++ Generar 1000000.R
@@ -0,0 +1,13 @@
+#Solución
+#
+
+#DISTRIBUCIÓN UNIFORME
+#Se utiliza el comando runif(N,min,max)
+
+N1000000<-1000000
+uniform1000000<-runif(N1000000,0,500)
+uniform1000000
+hist(uniform1000000)
+
+#RESUMEN ESTADÍSTICO
+summary(uniform1000000)
